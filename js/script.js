8d8dc8e996d35a62faaeea4e7ff69ad41e487421
gypsysVic <vkanturv@gmail.com>
2020-11-28 21:35:25 +0200

    First lesson about creationg objects and connection with user in a browser

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 "use strict";
 
-console.log("Hello World");
-const answers = [];
+//1 Excercise
+const numberOfFilms = +prompt("How many fils have you watched?", "");
+//document.getElementById("films").innerHTML = numberOfFilms;
 
-for(let i = 0; i < 2; i++){
-answers[i] = prompt("Enter your name", "");
-}
-console.log(typeof(answers));
+//2 Excercise
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {  
+    },
+    actors : {
+    },
+    genres : [],
+    privat : false
+}; 
+
+//3 Exsercice
+
+/*const filmName = prompt("Can you name the film you`ve watched recently?", ""),
+      filmRate = +prompt("Rate this film", ""),
+
+      filmName2 = prompt("Can you name the film you`ve watched recently?", ""),
+      filmRate2 = +prompt("Rate this film", "");
+
+    personalMovieDB.movies[filmName] = filmRate;
+    personalMovieDB.movies[filmName2] = filmRate2;*/
+
+    for(let i = 0; i < 2; i++)
+    {
+        const filmN = [];
+        const filmR = [];
+        filmN [i] = prompt("Can you name the film you`ve watched recently?", "");
+        filmR [i] = +prompt("Rate this film", "");
+        personalMovieDB.movies[filmN[i]] = filmR [i];       
+    }
+
+    console.log(personalMovieDB.movies); 
