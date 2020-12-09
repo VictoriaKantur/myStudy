c32bb18d2fb76e02cb33ac4a7e97955226e997c2
gypsysVic <vkanturv@gmail.com>
2020-12-09 13:23:36 +0200

    Methids inside objects and forEach

--- js/script.js
+++ js/script.js
@@ -1,82 +1,96 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-
-    numberOfFilms = +prompt("How many fils have you watched?", "");
-    
-    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("How many fils have you watched?", "");
-    }
-}
-
-start();
 
 let personalMovieDB;
+
 personalMovieDB = {
-    count : numberOfFilms,
+    count : 0,
     movies : {  
     },
     actors : {
     },
     genres : [],
-    privat : false
-}; 
+    privat : false,
 
-function showMyDB(hidden){
+    start : function() {
 
-    if(!hidden)
+        personalMovieDB.count = +prompt("How many fils have you watched?", "");
+        
+        while(personalMovieDB.count == "" || personalMovieDB.count == null || 
+        isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("How many fils have you watched?", "");
+        }
+    },
+
+    rememberMyFilms : function(){
+        for(let i = 0; i < 2; i++)
+        {
+            const a = prompt ('Which film you`ve watched recently?', ''),
+                  b = prompt('Rate it', '');
+            
+            if(a != null && b != null || a != '' || b != '' || a.length < 50){
+                personalMovieDB.movies[a] = b;
+                console.log('Done!');
+            } else {
+                console.log('Error!');
+                i--;
+            }
+        }
+    },
+
+    detectPersonalLevel : function(){
+        if(personalMovieDB.count < 10)
     {
-        console.log("Amount of films - " + personalMovieDB.count +
-        "movies - " + personalMovieDB.movies +
-        "actors - " + personalMovieDB.actors +
-        "genres - " + personalMovieDB.genres +
-        "privat value - " + personalMovieDB.privat);
-    }
-}
+        console.log("Not enought");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count3 < 30){
+        console.log("Wow, you`re cool");
+    } else if (personalMovieDB.count > 30){
+        console.log("You`re cinoholoc");
+    } else {
+        console.log("Error");
+    } 
+    },
 
-showMyDB(personalMovieDB.privat);
+    showMyDB : function (hidden){
 
-function writeYourGanres()
-{
+        if(!hidden)
+        {
+            console.log(personalMovieDB);
+        }
+    },
+
+    
+    writeYourGanres : function(){
     for(let i =0; i < 3; i++)
     {
         personalMovieDB.genres[i] = prompt("Can you name your favourite ganre number "+ (i+1) +"?", "");
-    }
-    console.log(personalMovieDB.genres);
-}
-
-writeYourGanres();
-
-if(personalMovieDB.count < 10)
-{
-    console.log("Not enought");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count3 < 30){
-    console.log("Wow, you`re cool");
-} else if (personalMovieDB.count > 30){
-    console.log("You`re cinoholoc");
-} else {console.log("Error");
-}  
-
-  /*for(let i = 0; i < 2; i++)
-    {
-
-        const filmN = prompt("Can you name the film you`ve watched recently?", 
-        "");
-        const filmR = +prompt("Rate this film", "");
-
-        if(filmN.length < 50 && filmN != null && filmR != null && 
-            filmN == "" && filmR == "")
+        if(personalMovieDB.genres[i] == null || personalMovieDB.genres[i] == '')
         {
-            personalMovieDB.movies[filmN] = filmR; 
-            console.log("Done"); 
-        } else {
-            console.log("Error"); 
             i--;
+            personalMovieDB.genres[i] = prompt("Can you name your favourite ganre number "+ (i+1) +"?", "");
         }
-              
     }
+    
+    personalMovieDB.genres.forEach((item, i, personalMovieDB) => {
+        console.log(`Favourite genre - ${item} has number ${i+1}
+        and it's an array ${personalMovieDB.genres}`);
+    });
+    },
+
+    toggleVisibleMyDB : function(){
+    
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = false;
+         } else{
+            personalMovieDB.privat =true;
+
+        }
+    }
+}; 
 
 
-*/
+
+
+
+
+  
---
+++ js/study.js
@@ -0,0 +1,13 @@
+"use sctrict";
+
+const soldier = {
+    health : 400,
+    armor : 100
+};
+
+const john = {
+    health : 100
+};
+
+john.__proto__ = soldier;
+console.log(john.armor);
